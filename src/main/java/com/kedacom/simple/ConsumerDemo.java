c6cf95349348bd822d1104e1568c3e9637f4708d
yingpeng93 <2291314224@qq.com>
2018-08-29 17:06:38 +0800

    添加了一些注释

--- src/main/java/com/kedacom/simple/ConsumerDemo.java
+++ src/main/java/com/kedacom/simple/ConsumerDemo.java
@@ -15,16 +15,25 @@ import java.util.Properties;
 public class ConsumerDemo {
     public static void main(String[] args) {
         Properties properties = new Properties();
+        //kafka的brokers代理集群
         properties.put("bootstrap.servers", "139.224.37.87:9092");
         properties.put("group.id", "group-1");
+        //是否自动提交
         properties.put("enable.auto.commit", "true");
         properties.put("auto.commit.interval.ms", "1000");
+        //earliest当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，从头开始消费
+        //latest 当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，消费新产生的该分区下的数据
+        //none
+        //topic各分区都存在已提交的offset时，从offset后开始消费；只要有一个分区不存在已提交的offset，则抛出异常
         properties.put("auto.offset.reset", "earliest");
+        //超时时间
         properties.put("session.timeout.ms", "30000");
+        //序列化
         properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        //订阅主题列表topic
         kafkaConsumer.subscribe(Arrays.asList("HelloWorld"));
         while (true) {
             ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
--- src/main/java/com/kedacom/simple/ProducerDemo.java
+++ src/main/java/com/kedacom/simple/ProducerDemo.java
@@ -17,9 +17,13 @@ public class ProducerDemo {
         //配置kafka集群地址
         properties.put("bootstrap.servers", "10.65.3.5:9092");
         //acks配置控制生产者请求下的标准是完全的。
+        //acks=0：如果设置为0，生产者不会等待kafka的响应。
+        //acks=1：这个配置意味着kafka会把这条消息写到本地日志文件中，但是不会等待集群中其他机器的成功响应。
+        //acks=all：这个配置意味着leader会等待所有的follower同步完成。这个确保消息不会丢失，除非kafka集群中所有机器挂掉。这是最强的可用性保证。
         properties.put("acks", "all");
-
+        //配置为大于0的值的话，客户端会在消息发送失败时重新发送。
         properties.put("retries", 0);
+        //当多条消息需要发送到同一个分区时，生产者会尝试合并网络请求。这会提高client和生产者的效率
         properties.put("batch.size", 16384);
         properties.put("linger.ms", 1);
         properties.put("buffer.memory", 33554432);
